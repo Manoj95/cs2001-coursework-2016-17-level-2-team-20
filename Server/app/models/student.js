4d6fd4c59e5c79539c1278f528c24119d680bf4a
CSOrion <CSOrion@users.noreply.github.com>
2017-02-26 00:46:40 +0000

    Init notes auth

--- Server/app/models/student.js
+++ Server/app/models/student.js
@@ -1,29 +1,83 @@
-// Get an instance of mongoose and mongoose.Schema
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 var Schema = mongoose.Schema;
 
 var noteSchema = new Schema({
-    date: { 
-		type: Date, 
-		required: true
-	},
-	title: { 
-		type: String, 
-		required: true,
-		index: true
-	},
-    text: { 
-		type: String, 
-		required: true
-	}
+  date: {
+    type: Date,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true,
+    index: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
 });
 
-// Set up a mongoose model and pass it using module.exports
-module.exports = mongoose.model('Student', new Schema({ 
-	student: { 
-		type: Number, 
-		required: true,
-		unique: true
-	},
-    notes: [noteSchema]
-}));
+var studentSchema = new Schema({
+  student: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  notes: [noteSchema]
+});
+
+//
+// // Set up a mongoose model and pass it using module.exports
+// module.exports = mongoose.model('Student', new Schema({
+// 	student: {
+// 		type: Number,
+// 		required: true,
+// 		unique: true
+// 	},
+//     notes: [noteSchema]
+// }));
+
+// Saves the user's password hashed (plain text password storage is not good)
+studentSchema.pre('save', function (next) {
+  var user = this;
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+studentSchema.methods.comparePassword = function(password, callback) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, isMatch);
+  });
+};
+
+var Student = module.exports = mongoose.model('Student', studentSchema);
+
+module.exports.getStudents = function(callback, limit) {
+  Student.find(callback).limit(limit);
+}
+
+module.exports.getNotes = function(callback, limit) {
+  Student.find(callback).limit(limit);
+}
--- Server/server.js
+++ Server/server.js
@@ -3,22 +3,21 @@ var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
-var path = __dirname + '/views/';
-
-// Used to create, sign, and verify tokens
+var passport = require('passport');
 var jwt = require('jsonwebtoken');
+var config = require('./config/config');
+var port = process.env.PORT || 8080;
 
-// Get our config file
-var config = require('./config');
+// Webpage
+app.use(express.static('app/views'))
 
 // Get our mongoose model
 var Student = require('./app/models/student');
 
-// Used to create, sign, and verify tokens
-var port = process.env.PORT || 8080;
-
 // Connect to database
 mongoose.connect(config.database);
+app.use(passport.initialize())
+require('./config/passport')(passport);
 
 // Use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,26 +25,13 @@ app.use(bodyParser.json());
 
 // Use morgan to log requests to the console
 app.use(morgan('dev'));
-app.use(express.static('views'))
 
 app.get('/', function(req, res) {
-    res.send('API @ http://localhost:' + port + '/api');
+  res.send('API @ http://localhost:' + port + '/api');
 });
 
 app.get('/brunelplanner', function(req, res) {
-    res.sendFile(path + "index.html");
-});
-
-app.get('/setup', function(req, res) {
-
-  var student = new Student({
-    student: 1111111
-  });
-
-  student.save(function(err) {
-    if (err) throw err;
-    res.json({ success: true });
-  });
+  res.sendFile(__dirname + '/app/views/index.html');
 });
 
 var routes = express.Router();
@@ -54,10 +40,66 @@ routes.get('/', function(req, res) {
   res.json({ message: 'Notes API' });
 });
 
+routes.post('/register', function(req, res) {
+  if(!req.body.student || !req.body.password) {
+    res.json({ success: false, message: 'Please enter student id and password.' });
+  } else {
+    var newStudent = new Student({
+      student: req.body.student,
+      password: req.body.password
+    });
+
+    // Attempt to save the user
+    newStudent.save(function(err) {
+      if (err) {
+        return res.json({ success: false, message: 'That student already exists.'});
+      }
+      res.json({ success: true, message: 'Successfully created new student.' });
+    });
+  }
+});
+
+routes.post('/auth', function(req, res) {
+  Student.findOne({
+    student: req.body.student
+  }, function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.send({ success: false, message: 'Authentication failed. Student not found.' });
+    } else {
+      // Check if password matches
+      user.comparePassword(req.body.password, function(err, isMatch) {
+        if (isMatch && !err) {
+          // Create token if the password matched and no error was thrown
+          var token = jwt.sign(user, config.secret, {
+            expiresIn: 10080 // in seconds
+          });
+          res.json({ success: true, token: 'JWT ' + token });
+        } else {
+          res.send({ success: false, message: 'Authentication failed. Passwords did not match.' });
+        }
+      });
+    }
+  });
+});
+
 routes.get('/students', function(req, res) {
-  Student.find({}, function(err, students) {
+  Student.getStudents(function(err, students) {
+    if (err) throw err;
     res.json(students);
   });
+});
+
+routes.get('/notes', function(req, res) {
+  Student.getStudents(function(err, students) {
+    if (err) throw err;
+    res.json(students);
+  });
+});
+
+routes.get('/auth', function(req, res) {
+  // ...
 });
 
 app.use('/api', routes);
