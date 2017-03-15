29c7334dccb24ec95d2ee6a3eea7d944621c6b6f
CSOrion <CSOrion@users.noreply.github.com>
2017-03-15 22:18:02 +0000

    Create week dialog fragment

--- Brunel-Planner/app/src/main/java/g20/brunelplanner/views/activities/fragments/TimetableFragment.java
+++ Brunel-Planner/app/src/main/java/g20/brunelplanner/views/activities/fragments/TimetableFragment.java
@@ -3,6 +3,7 @@ package g20.brunelplanner.views.activities.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -18,14 +20,15 @@ import butterknife.ButterKnife;
 import g20.brunelplanner.R;
 import g20.brunelplanner.controllers.databases.RealmController;
 import g20.brunelplanner.models.planner.Timetable;
-import g20.brunelplanner.utils.CurrentWeek;
 import g20.brunelplanner.views.activities.adapters.TimetableAdapter;
 
 
-public class TimetableFragment extends Fragment {
+public class TimetableFragment extends Fragment implements WeekDialogFragment.WeekDialogListener {
 
     @BindView(R.id.recycler_view)
     RecyclerView recyclerView;
+
+    private int week;
 
     protected RealmController realmController;
 
@@ -33,16 +36,36 @@ public class TimetableFragment extends Fragment {
         // ...
     }
 
-    private void setUpRecyclerView(int week) {
+    private void setUpRecyclerView() {
         // There may be a better way to init this
         realmController = RealmController.getInstance();
+
+        try {
+            ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Week " + week);
+        } catch (NullPointerException e) {
+            Log.e("TimetableFragment", "onCreateView: ", e);
+        }
 
         // This is needed for some reason
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
         // Set the adapter for the recycler view and populate with the realm data
-        recyclerView.setAdapter(new TimetableAdapter(this,
-                realmController.getRealm().where(Timetable.class).equalTo("weeks.val", week).findAllAsync()));
+        recyclerView.setAdapter(new TimetableAdapter(this, realmController.getRealm()
+                        .where(Timetable.class)
+                        .equalTo("weeks.val", week)
+                        .findAllAsync()));
         recyclerView.setHasFixedSize(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case R.id.action_change_week:
+                changeWeek();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -53,15 +76,7 @@ public class TimetableFragment extends Fragment {
         ButterKnife.bind(this, view);
         setHasOptionsMenu(true);
 
-        int currentWeek = CurrentWeek.getCurrentWeek();
-
-        try {
-            ((AppCompatActivity)getActivity()).getSupportActionBar().setSubtitle("Week " + currentWeek);
-        } catch (NullPointerException e) {
-            Log.e("TimetableFragment", "onCreateView: ", e);
-        }
-
-        setUpRecyclerView(currentWeek);
+        setUpRecyclerView();
 
         return view;
 
@@ -79,5 +94,17 @@ public class TimetableFragment extends Fragment {
         realmController.closeRealm();
     }
 
+    public void changeWeek() {
+        FragmentManager fm = getFragmentManager();
+        WeekDialogFragment weekDialogFragment = new WeekDialogFragment();
+        weekDialogFragment.setTargetFragment(TimetableFragment.this, 300);
+        weekDialogFragment.show(fm, "week_dialog");
+    }
+
+    @Override
+    public void onFinishWeekDialog(int week) {
+        this.week = week;
+        setUpRecyclerView();
+    }
 
 }
---
+++ Brunel-Planner/app/src/main/java/g20/brunelplanner/views/activities/fragments/WeekDialogFragment.java
@@ -0,0 +1,60 @@
+package g20.brunelplanner.views.activities.fragments;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class WeekDialogFragment extends DialogFragment {
+
+    int currentWeek;
+
+    public WeekDialogFragment() {
+        // Empty constructor required for DialogFragment
+    }
+
+    // Defines the listener interface
+    public interface WeekDialogListener {
+        void onFinishWeekDialog(int week);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        ArrayList<Integer> weekList = new ArrayList<>();
+
+        for (int i = 0; i < 52; i++) {
+            weekList.add(i);
+        }
+
+        builder.setTitle("Change Week")
+                .setPositiveButton("Set", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.d("Dialog", "onClick: Fire");
+                        sendBackResult(currentWeek);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.d("Dialog", "onClick: Cancel");
+                    }
+                });
+//        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+    public void sendBackResult(int week) {
+        // Notice the use of `getTargetFragment` which will be set when the dialog is displayed
+        WeekDialogListener listener = (WeekDialogListener) getTargetFragment();
+        listener.onFinishWeekDialog(week);
+        dismiss();
+    }
+
+
+}
