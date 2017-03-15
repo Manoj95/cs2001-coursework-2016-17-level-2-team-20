3a4014fb095f8f3e0d63507fb5eabc2135c1e3ee
Manoj95 <manojchandrakumar@hotmail.com>
2017-03-15 16:19:08 +0000

    Added everything to the fragment to make it work. Just need to connect the shared prefs to the actual components.

--- Brunel-Planner/app/src/main/java/g20/brunelplanner/views/activities/fragments/SettingsFragment.java
+++ Brunel-Planner/app/src/main/java/g20/brunelplanner/views/activities/fragments/SettingsFragment.java
@@ -4,7 +4,6 @@ package g20.brunelplanner.views.activities.fragments;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.xdty.preference.colorpicker.ColorPickerDialog;
 import org.xdty.preference.colorpicker.ColorPickerSwatch;
@@ -24,9 +24,22 @@ import butterknife.ButterKnife;
 import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 import g20.brunelplanner.R;
+
+import static org.xdty.preference.colorpicker.ColorPickerDialog.newInstance;
+
 public class SettingsFragment extends Fragment {
 
+
     private int mSelectedColor, actionBarColour, BackGroundColour, CardsColour, FontColour, HeaderColour;
+
+
+
+    private int DefaultActionBarColour = R.color.colorPrimary;
+    private int DefaultBackGroundColour = R.color.cardview_light_background;
+    private int DefaultCardsColour = R.color.cardview_light_background;
+    private int DefaultFontColour = R.color.colorPrimaryDark;
+    private int DefaultHeaderColour = R.color.colorAccent;
+    private boolean DefaultNotificationChose = true;
 
     @BindView(R.id.settings_actionbar_text_view)
     TextView actionbar_text_view;
@@ -57,8 +70,13 @@ public class SettingsFragment extends Fragment {
     @BindView(R.id.settings_notification_checkbox)
     CheckBox notification_checkbox;
 
-    @BindView(R.id.nav_view)
-    NavigationView navigationView;
+
+    @OnCheckedChanged(R.id.settings_notification_checkbox)
+    void onChecked(boolean checked){
+        saveNotificationPrefs("Notification", checked);
+        Toast.makeText(getActivity(), checked ? "Checked!" : "Unchecked!",Toast.LENGTH_SHORT).show();
+    }
+
 
     public SettingsFragment() {
 
@@ -73,79 +91,199 @@ public class SettingsFragment extends Fragment {
         } catch (NullPointerException e) {
             Log.e("SettingsFragment", "onCreateView: ", e);
         }
-        //ButterKnife.bind(this,view); <------ This doesn't work. Causes the app to crash if the settings option is chosen.
-        //ButterKnife.bind(view); <----------- The app doesn't crash but the onclick methods don't work.
-
+        ButterKnife.bind(this,view);
+        LoadPreviousPrefs();
         return view;
     }
 
 
-
-    public void ColourPicker(){ //<---This works.
-        mSelectedColor = ContextCompat.getColor(getActivity().getBaseContext(), R.color.flamingo);
-        int[] mColors = getResources().getIntArray(R.array.default_rainbow);
-        ColorPickerDialog dialog = ColorPickerDialog.newInstance(R.string.color_picker_default_title,
-                mColors,
-                mSelectedColor,
-                5, // Number of columns
-                ColorPickerDialog.SIZE_SMALL);
-        dialog.setOnColorSelectedListener(new ColorPickerSwatch.OnColorSelectedListener() {
-            @Override
-            public void onColorSelected(int color) {
-                mSelectedColor = color;
-            }
-        });
-        dialog.show((getActivity()).getFragmentManager(), "color_dialog_test");
+            @OnClick(R.id.store_settings)
+    public void StoreSettings(View v) {
+        Toast.makeText(getActivity(), "All Saved", Toast.LENGTH_LONG).show();
     }
-
-
-        @OnClick(R.id.store_settings)
-        public void StoreSettings(View v) {
-           saveIntPrefs("ActionBarColour", actionBarColour);
-            saveIntPrefs("BackGroundColour", BackGroundColour);
-            saveIntPrefs("CardColour", CardsColour);
-            saveIntPrefs("FontColour", FontColour);
-            saveIntPrefs("HeaderColour", HeaderColour);
-        }
-
-       @OnClick(R.id.settings_actionbar_button)
-        public void ActionBarColour(View v) {
-           ColourPicker();
-           actionBarColour = mSelectedColor;
-        }
-
-        @OnClick(R.id.settings_background_button)
-        public void BackgroundColour(View v) {
-            ColourPicker();
-            BackGroundColour = mSelectedColor;
-        }
-
-        @OnClick(R.id.settings_cards_button)
-        public void CardsColour(View v) {
-            ColourPicker();
-            CardsColour = mSelectedColor;
-        }
-
-        @OnClick(R.id.settings_font_button)
-        public void FontColour(View v) {
-            ColourPicker();
-            FontColour = mSelectedColor;
-        }
 
 
     @OnClick(R.id.settings_header_button)
     public void HeaderColour(View v) {
-        ColourPicker();
-        HeaderColour = mSelectedColor;
+
+        mSelectedColor = ContextCompat.getColor(getActivity(), DefaultHeaderColour);
+        int[] mColors = getResources().getIntArray(R.array.rainbow);
+
+        ColorPickerDialog dialog = newInstance(R.string.color_picker_default_title,
+                mColors,
+                mSelectedColor,
+                4, // Number of columns
+                ColorPickerDialog.SIZE_SMALL);
+
+        dialog.setOnColorSelectedListener(new ColorPickerSwatch.OnColorSelectedListener() {
+
+            @Override
+            public void onColorSelected(int color) {
+                mSelectedColor = color;
+                header_text_view.setTextColor(mSelectedColor);
+                saveColourPrefs("HeaderColour", mSelectedColor);
+            }
+
+        });
+
+        dialog.show(getActivity().getFragmentManager(), "color_dialog_test");
+
+    }
+
+    @OnClick(R.id.settings_actionbar_button)
+    public void ActionBarColour(View v) {
+        mSelectedColor = ContextCompat.getColor(getActivity(), DefaultActionBarColour);
+        int[] mColors = getResources().getIntArray(R.array.rainbow);
+
+        ColorPickerDialog dialog = newInstance(R.string.color_picker_default_title,
+                mColors,
+                mSelectedColor,
+                4, // Number of columns
+                ColorPickerDialog.SIZE_SMALL);
+
+        dialog.setOnColorSelectedListener(new ColorPickerSwatch.OnColorSelectedListener() {
+
+            @Override
+            public void onColorSelected(int color) {
+                mSelectedColor = color;
+                actionbar_text_view.setTextColor(mSelectedColor);
+                saveColourPrefs("ActionBarColour", mSelectedColor);
+            }
+
+        });
+
+        dialog.show(getActivity().getFragmentManager(), "color_dialog_test");
+
+    }
+
+    @OnClick(R.id.settings_background_button)
+    public void BackgroundColour(View v) {
+        mSelectedColor = ContextCompat.getColor(getActivity(), DefaultBackGroundColour);
+        int[] mColors = getResources().getIntArray(R.array.rainbow);
+
+        ColorPickerDialog dialog = newInstance(R.string.color_picker_default_title,
+                mColors,
+                mSelectedColor,
+                4, // Number of columns
+                ColorPickerDialog.SIZE_SMALL);
+
+        dialog.setOnColorSelectedListener(new ColorPickerSwatch.OnColorSelectedListener() {
+
+            @Override
+            public void onColorSelected(int color) {
+                mSelectedColor = color;
+                background_text_view.setTextColor(mSelectedColor);
+                saveColourPrefs("BackGroundColour", mSelectedColor);
+            }
+
+        });
+
+        dialog.show(getActivity().getFragmentManager(), "color_dialog_test");
+
+    }
+
+    @OnClick(R.id.settings_cards_button)
+    public void CardsColour(View v) {
+        mSelectedColor = ContextCompat.getColor(getActivity(), DefaultCardsColour);
+        int[] mColors = getResources().getIntArray(R.array.rainbow);
+
+        ColorPickerDialog dialog = newInstance(R.string.color_picker_default_title,
+                mColors,
+                mSelectedColor,
+                4, // Number of columns
+                ColorPickerDialog.SIZE_SMALL);
+
+        dialog.setOnColorSelectedListener(new ColorPickerSwatch.OnColorSelectedListener() {
+
+            @Override
+            public void onColorSelected(int color) {
+                mSelectedColor = color;
+                cards_text_view.setTextColor(mSelectedColor);
+                saveColourPrefs("CardsColour", mSelectedColor);
+            }
+
+        });
+
+        dialog.show(getActivity().getFragmentManager(), "color_dialog_test");
+
+    }
+
+    @OnClick(R.id.settings_font_button)
+    public void FontColour(View v) {
+        mSelectedColor = ContextCompat.getColor(getActivity(), DefaultFontColour);
+        int[] mColors = getResources().getIntArray(R.array.rainbow);
+
+        ColorPickerDialog dialog = newInstance(R.string.color_picker_default_title,
+                mColors,
+                mSelectedColor,
+                4, // Number of columns
+                ColorPickerDialog.SIZE_SMALL);
+
+        dialog.setOnColorSelectedListener(new ColorPickerSwatch.OnColorSelectedListener() {
+
+            @Override
+            public void onColorSelected(int color) {
+                mSelectedColor = color;
+                font_text_view.setTextColor(mSelectedColor);
+                saveColourPrefs("FontColour", mSelectedColor);
+            }
+
+        });
+
+        dialog.show(getActivity().getFragmentManager(), "color_dialog_test");
+
     }
 
 
 
-    private void saveIntPrefs(String ValueOfKey, int ValueToSave){
+
+    private void saveColourPrefs(String ValueOfKey, int ValueToSave){
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
         SharedPreferences.Editor edit = sharedPreferences.edit();
         edit.putInt(ValueOfKey, ValueToSave);
         edit.commit();
     }
+    private void saveNotificationPrefs(String ValueOfKey, Boolean ValueToSave){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        SharedPreferences.Editor edit = sharedPreferences.edit();
+        edit.putBoolean(ValueOfKey, ValueToSave);
+        edit.commit();
+    }
+    private int loadColourPrefs(String ValueOfKey, int DefaultValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.
+                getDefaultSharedPreferences(getActivity());
+        int StoredColour = sharedPreferences.
+                getInt(ValueOfKey, DefaultValue);
+        return StoredColour;
 
+    }
+    private boolean loadNotificationPrefs(String ValueOfKey, boolean DefaultValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.
+                getDefaultSharedPreferences(getActivity());
+        boolean StoredColour = sharedPreferences.
+                getBoolean(ValueOfKey, DefaultValue);
+        return StoredColour;
+
+    }
+
+
+    public void LoadPreviousPrefs(){
+        int PreHeaderColour = loadColourPrefs("HeaderColour",DefaultHeaderColour);
+        header_text_view.setTextColor(PreHeaderColour);
+
+        int PreFontColour = loadColourPrefs("FontColour",DefaultFontColour);
+        font_text_view.setTextColor(PreFontColour);
+
+        int PreBackGroundColour = loadColourPrefs("BackGroundColour",DefaultBackGroundColour);
+        background_text_view.setTextColor(PreBackGroundColour);
+
+        int PreActionBarColour = loadColourPrefs("ActionBarColour",DefaultActionBarColour);
+        actionbar_text_view.setTextColor(PreActionBarColour);
+
+        int PreCardsColour = loadColourPrefs("CardsColour",DefaultCardsColour);
+        cards_text_view.setTextColor(PreCardsColour);
+
+        boolean PreNotificationChose = loadNotificationPrefs("Notification",DefaultNotificationChose);
+        notification_checkbox.setChecked(PreNotificationChose);
+    }
 }
