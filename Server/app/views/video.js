880adc8101b2b719f5a90e4bc695e6095e3f4f01
guba1a <1514850@my.brunel.ac.uk>
2017-03-20 13:30:12 +0000

    Added feature descs

--- Server/app/views/video.js
+++ Server/app/views/video.js
@@ -2,12 +2,16 @@ var video = document.getElementById('video');
 var source = document.createElement('source');
 
 function playTimetable() {
+  hideAll();
+  $( ".timetable" ).toggle();
   source.setAttribute('src', 'videos/compressed_timetable.mp4');
   video.appendChild(source);
   play()
 }
 
 function playMaps() {
+  hideAll();
+  $( ".maps" ).toggle();
   source.setAttribute('src', 'videos/compressed_maps.mp4');
   video.appendChild(source);
   play()
@@ -17,4 +21,9 @@ function play() {
   video.pause();
   video.load();
   video.play();
+}
+
+function hideAll() {
+  $( ".timetable" ).toggle(false);
+  $( ".maps" ).toggle(false);
 }
